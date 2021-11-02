24b8393245d7c0b07de6e1e0e3fd23ac77f7a150
Piyush Rai <piyrai1@publicisgroupe.net>
2021-11-03 00:19:50 +0530

    implemented CRUD functionalities

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,68 +1,23 @@
 import React from "react";
-import { Form, Field } from "react-final-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
+import StreamForm from "./StreamForm";
+class StreamCreate extends React.Component {
 
 
-const StreamCreate = (props) => {
-    
-  const renderError = ({ error, touched }) => {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  };
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  }
 
-  const renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.touched && meta.error ? "error" : ""}`;
+  render(){
     return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {renderError(meta)}
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit}/>
       </div>
     );
-  };
+  }
 
-  const validate = (formValues) => {
-    const errors = {};
+}
 
-    if (!formValues.title) {
-      errors.title = "You must enter a title";
-    }
-
-    if (!formValues.description) {
-      errors.description = "You must enter a description";
-    }
-
-    return errors;
-  };
-
-  const onSubmit = (formValues) => {
-    console.log(formValues);
-  };
-
-  return (
-    <Form
-      initialValues={props.initialValues}
-      onSubmit={onSubmit}
-      validate={validate}
-      render={({ handleSubmit }) => {
-        return (
-          <form onSubmit={handleSubmit} className="ui form error">
-            <Field name="title" component={renderInput} label={"Enter Title"} />
-            <Field
-              name="description"
-              component={renderInput}
-              label="Enter Description"
-            />
-            <button className="ui button primary">Submit</button>
-          </form>
-        );
-      }}
-    />
-  );
-};
-
-export default StreamCreate;
+export default connect(null, {createStream})(StreamCreate);
